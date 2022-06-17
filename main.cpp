c5545ba04013977f2ef15df1becbc7e5dd8b8f12
BrilianPermataZulfa <101861091+BrilianPermataZulfa@users.noreply.github.com>
2022-06-17 14:45:40 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "library/Class.h"
+#include "library/Input.h"
+#include "library/Proses.h"
+#include "library/Output.h"
+
+int main() {
+  ArrayMatrik x;
+  x.input_data();
+  x.proses_data();
+  x.output_data();
+  return 0;
+}
