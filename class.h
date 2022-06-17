9f64cf18242789efe69a8b4e91097bdc8725976f
BrilianPermataZulfa <101861091+BrilianPermataZulfa@users.noreply.github.com>
2022-06-17 14:52:46 +0700

    Create class.h

---
+++ class.h
@@ -0,0 +1,12 @@
+using namespace std;
+class ArrayMatrik {
+  public:
+    void input_data();
+    void proses_data();
+    void output_data();
+
+  private:
+    string array[75];
+    string matrik[15][5];
+    int byk_data,index;
+};
